e63f55d7f6814e05ee760509a5b084ca763419f5
narayanspg <31915260+narayanspg@users.noreply.github.com>
2025-03-05 19:45:26 +0530

    Create run-perf-tests.sh
    created perf-test.sh script for running performance tests.

---
+++ scripts/odf/run-perf-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+##
+## This script runs Performance test in the remote bastion node
+##
+
+# Change to the correct directory once
+cd ${WORKSPACE}/ocs-upi-kvm/scripts/ || { echo "Error: Directory not found"; exit 1; }
+
+# Source environment variables
+. ${WORKSPACE}/env_vars.sh
+
+# Loop over both test types (FILE and BLOCK)
+for PERF_TYPE in FILE BLOCK; do
+    # Run FIO test and log output
+    ./run-fio.sh --tier ${PERF_TYPE} > performance_${PERF_TYPE}.log
+
+    # Extract the last numerical value (FILE_NUM) from the log
+    FILE_NUM=$(awk '/Fio results directory:/ {val=$NF} END {print val}' performance_${PERF_TYPE}.log)
+
+    # Check if FILE_NUM is empty
+    if [[ -z "$FILE_NUM" ]]; then
+        echo "Error: Unable to extract FILE_NUM from performance_${PERF_TYPE}.log."
+        exit 1
+    fi
+
+    # Run fio-report.sh with extracted FILE_NUM
+    ./fio-report.sh ${PERF_TYPE} "$FILE_NUM" > performance-${PERF_TYPE}-report.log
+done
