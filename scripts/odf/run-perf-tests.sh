5ffc109a0e87ffa7fc5313dada00208e48489e7c
narayanspg <31915260+narayanspg@users.noreply.github.com>
2025-03-07 13:25:46 +0530

    Update run-perf-tests.sh
    updated file and block option

--- scripts/odf/run-perf-tests.sh
+++ scripts/odf/run-perf-tests.sh
@@ -12,7 +12,7 @@ cd ${WORKSPACE}/ocs-upi-kvm/scripts/ || { echo "Error: Directory not found"; exi
 . ${WORKSPACE}/env_vars.sh
 
 # Loop over both test types (file and block)
-for PERF_TYPE in FILE BLOCK; do
+for PERF_TYPE in file block; do
     # Run FIO test and log output
     ./run-fio.sh ${PERF_TYPE} > performance_${PERF_TYPE}.log 2>&1
 
